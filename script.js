752c1b46f01b00017e6d4aad1169fd916b5d3d8c
Hashininirasha <hashininirashamunasinghe@gmail.com>
2023-03-28 10:26:33 +0530

    added add_repo function and btn event listner

--- script.js
+++ script.js
@@ -1,20 +1,30 @@
+
 const input = document.querySelector('input')
 const btn = document.querySelector('button')
 const card = document.querySelector('.card')
+const API ="https://api.github.com/users/";
 
 const repos_container = document.querySelector('.repos')
 
-const API ="https://api.github.com/users/";
-
 const user = async (username) => {
+    try{
     const resp = await fetch(`${API}${username}`)
     const respData = await resp.json()
-    return respData
-}
+    return respData;
+    }catch(err){
+        console.log(err)
+    }
+};
 
 const repos = async (username) => {
-    const resp = await fetch(`${API}${username}`)
+    try{
+    const resp = await fetch(`${API}${username}/repos`)
     const respData = await resp.json()
 
-    return respData
+    return respData;
+    }catch(err){
+        console.log(err);
+    }
 }
+
+
