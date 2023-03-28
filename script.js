ec88d2e93e08bd23d05ae38610c5f9d48da65e2d
Hashininirasha <hashininirashamunasinghe@gmail.com>
2023-03-28 10:26:59 +0530

    added add_repo function and btn event listner

--- script.js
+++ script.js
@@ -27,4 +27,51 @@ const repos = async (username) => {
     }
 }
 
+async function add_repo () {
+    const reposData = await repos(input.value)
+    console.log(reposData);
+    repos_container.innerHTML = reposData.map(repo => {
+        return `
+            <div class="card">
+                <h2>${repo.name}</h2>
+                <a href="${repo.clone_url}" target="_blank">Take a look at this repo ></a>
+            </div>
+        `
+    }).join('')
+}
 
+btn.addEventListener('click', async () => {
+    const input_val = input.value
+    const search_result = await user(input_val)
+
+    add_repo()
+
+    if (!search_result.login) {
+        alert('No user found!')
+    } else {
+        card.innerHTML = `
+            <div class="avatar">
+                <img src="${search_result.avatar_url}" alt="">
+            </div>
+            <div class="info">
+                <h2>${search_result.name}</h2>
+                <p>${search_result.login}</p>
+                <div class="follow-info">
+                    <div class="single">
+                        <span>${search_result.followers}</span>
+                        <span>Followers</span>
+                    </div>
+                    <div class="single">
+                        <span>${search_result.following}</span>
+                        <span>Following</span>
+                    </div>
+                </div>
+                <div class="single">
+                    <span>${search_result.public_repos}</span>
+                    <span>Repos</span>
+                </div>
+            </div>
+            <a href="${search_result.html_url}" target="_blank">Visit Github Profile ></a>
+        `
+    }
+})
